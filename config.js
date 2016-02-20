0e8cba2c24bee9e2f28c317435f83806734cd961
Ömer Karışman <omerkarisman@gmail.com>
2016-02-20 12:34:31 +0200

    Added Auth

--- config.js
+++ config.js
@@ -17,5 +17,13 @@ module.exports = {
   },
   "humanReadableOutput": true,
   "collectionOutputType": "json",
-  "urlPrefix": ""
+  "urlPrefix": "",
+  "auth": {
+    "usersDBConnection": "mongodb://getir2:Kklmfux0r,@ds011248.mongolab.com:11248",
+    "usersCollection": "User",
+    "tokenDBConnection": "mongodb://getir2:Kklmfux0r,@ds011248.mongolab.com:11248",
+    "tokensCollectionName": "Token",
+    "universalAuthToken": "a3d38c88a4f5f21df86b810ff5235788",
+    "tokenExpirationTimeHours": 8
+}
 }
