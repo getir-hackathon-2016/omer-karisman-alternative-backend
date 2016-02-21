013855e4a6cfd28ab8bbcb54a7b957cbbc25a2f5
Ömer Karışman <omerkarisman@gmail.com>
2016-02-21 21:13:37 +0200

    Changed MongoLab DB to free

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ var logger = new (winston.Logger)({
 
 module.exports = {
 	"logger" : logger,
-  "db": "mongodb://getir2:Kklmfux0r,@ds013838-a0.mlab.com:13838/getir-hackathon",
+  "db": "mongodb://getir2:Kklmfux0r,@ds011248.mongolab.com:11248",
   "server": {
     "port": process.env.PORT || 3000,
     "address": "0.0.0.0"
@@ -29,9 +29,9 @@ module.exports = {
   "collectionOutputType": "json",
   "urlPrefix": "",
   "auth": {
-    "usersDBConnection": "mongodb://getir2:Kklmfux0r,@ds013838-a0.mlab.com:13838/getir-hackathon",
+    "usersDBConnection": "mongodb://getir2:Kklmfux0r,@ds011248.mongolab.com:11248/getir-hackathon",
     "usersCollection": "User",
-    "tokenDBConnection": "mongodb://getir2:Kklmfux0r,@ds013838-a0.mlab.com:13838/getir-hackathon",
+    "tokenDBConnection": "mongodb://getir2:Kklmfux0r,@ds011248.mongolab.com:11248/getir-hackathon",
     "tokensCollectionName": "Token",
     "universalAuthToken": "a3d38c88a4f5f21df86b810ff5235788",
     "tokenExpirationTimeHours": 8
